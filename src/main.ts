9d70061c1a2a72ded2ecb47aff62a4ab99978004
lucky-bruce <jalicorich@gmail.com>
2020-05-05 10:16:06 +0300

    chore: swagger, validation pipe install

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors(); // TODO: just for dev only
+
+  const options = new DocumentBuilder()
+    .setTitle('Forum API')
+    .addBearerAuth()
+    .setDescription('The forum api documentation')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
